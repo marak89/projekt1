beed63f9f5b7829d64bb1a2f52e177e02f4f1f01
Marcin Ral <marak89@marak89.com>
2011-10-17 15:00:27 +0200

    lista

--- Server.java
+++ Server.java
@@ -3,14 +3,8 @@ import java.rmi.Naming;
 import java.rmi.RemoteException;
 import java.rmi.RMISecurityManager;
 import java.rmi.server.UnicastRemoteObject;
-<<<<<<< HEAD
-
-import java.rmi.AlreadyBoundException;
 import java.rmi.*;
 import java.rmi.registry.*; 
-=======
-import java.rmi.*; 
->>>>>>> 8fdac6c0a5f0ebb88fc4a10b6cce089b09c33eba
  
 public class Server extends UnicastRemoteObject implements ServerIntf {
 
@@ -21,13 +15,6 @@ public class Server extends UnicastRemoteObject implements ServerIntf {
     public String getMessage() {
         return "Helou :)";
     }
-
-    public String odwrocString(String str) {
-	String odwr = new StringBuffer(str).reverse().toString();
-	return odwr;
-    }    
-    
-<<<<<<< HEAD
    
  public static void main(String args[]) throws RemoteException {
 
@@ -42,18 +29,3 @@ public class Server extends UnicastRemoteObject implements ServerIntf {
 
     } // public static void main
 } // public class Server
-=======
-    public static void main(String args[]) throws RemoteException {
-	
-//	LocateRegistry.createRegistry(1099);
-	
-        try {
-	   // Server obj = new Server();
-	    Naming.bind("//localhost/some_name");
-        }
-        catch(AlreadyBoundException ex) {
-            System.out.print(ex.getMessage());
-        }
-    }
-}
->>>>>>> 8fdac6c0a5f0ebb88fc4a10b6cce089b09c33eba
--- ServerIntf.java
+++ ServerIntf.java
@@ -3,5 +3,4 @@ import java.rmi.RemoteException;
  
 public interface ServerIntf extends Remote {
     public String getMessage() throws RemoteException;
-    public String odwrocString(String str) throws RemoteException;
 }
